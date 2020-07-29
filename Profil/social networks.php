18aad433da596b0ecaeec19869c45d9ad1e59a4a
Tom Tempelaere <ttempelaere@idaia.group>
2020-07-30 00:26:20 +0200

    add profil

---
+++ Profil/social networks.php
@@ -0,0 +1,11 @@
+<?php 
+
+$linkedin = "https://www.linkedin.com/in/tom-tempelaere-6a3b10171/"
+
+$instagram = "https://www.instagram.com/tomtempelaere/"
+
+$behance = "https://www.behance.net/tomtempelac825"
+
+?>
+
+
